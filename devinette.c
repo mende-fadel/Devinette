24e4ff53af7c1942d1a4db83de29d0d3e025061a
Fadel MENDE <fadel.mende@etu.unistra.fr>
2024-02-28 20:21:11 +0100

    ecriture de la fonction jeuDevinette() du fichier devinette.c

--- devinette.c
+++ devinette.c
@@ -4,5 +4,34 @@
 #include "devinette.h"
 
 void jeuDevinette(){
+    srand(time(NULL));
+    int secretNumber = rand() % 100 + 1;
+    int madevinette;
+    int suppositionPrecedente = -1;
+    int essais = 0;
+
+    printf("Bienvenue au jeu de devinettes !\n");
+    printf("Devinez le nombre entre 1 et 100.\n");
+
+    do{
+        printf("Entrez votre devinette : ");
+        scanf("%d", &madevinette);
+        essais++;
+
+        if(madevinette == suppositionPrecedente){
+            printf("Ce nombre a été déjà deviné !\n");
+            continue;
+        }
+        if(madevinette > suppositionPrecedente){
+            printf("Trop grand !\n");
+        } else if(madevinette < suppositionPrecedente){
+            printf("Trop petit !\n");
+        }
+
+        suppositionPrecedente = madevinette;
+    } while (madevinette != suppositionPrecedente);
+
+    printf("Bravo ! Vous avez deviné le nombre en %d essais .\n", essais);
+    
     
 }
